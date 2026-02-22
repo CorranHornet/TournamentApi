718468e8c9da5d16b56cde612f5efdd131f87dba
CorranHornet <texhex14@outlook.com>
2026-02-22 14:54:47 +0100

    feature/swagger: setup Tournament API controller and Swagger
    - Added TournamentsController with basic GET endpoint
    - Configured Program.cs to AddControllers and MapControllers
    - Removed conflicting OpenApi package to fix runtime errors
    - Swagger now works with /api/tournaments endpoint

---
+++ Controllers/TournamentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentApi.Models;
+using System.Collections.Generic;
+
+namespace TournamentApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TournamentsController : ControllerBase
+    {
+        // In-memory storage för demo / Swagger-test
+        private static List<Tournament> _tournaments = new();
+        private static int _nextId = 1;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_tournaments);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Tournament tournament)
+        {
+            tournament.Id = _nextId++;
+            _tournaments.Add(tournament);
+            return Ok(tournament);
+        }
+    }
+}
