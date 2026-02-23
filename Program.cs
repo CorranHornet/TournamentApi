748d1256489c993bdd9d9b7334e26112a84b3a7d
CorranHornet <texhex14@outlook.com>
2026-02-23 12:31:58 +0100

    Revert "feat: register AppDbContext in DI container"
    This reverts commit 3199b136eef84221bd5a3caa09f91cb0352adb30.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using TournamentApi.Data;
 
 
 
@@ -14,10 +12,6 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();   // Provides Swagger with information about available endpoints
 builder.Services.AddSwaggerGen();              // Generates interactive documentation and allows API testing via Swagger UI
-
-// Add AppDbContext to the dependency injection container
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 
