d15979739ea686b4f57e8a18957a535babbfe68f
CorranHornet <texhex14@outlook.com>
2026-03-23 10:57:27 +0100

    Add Game DTOs for CRUD (Create, Update, Response)

---
+++ Dtos/GameCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TournamentApi.Dtos
+{
+    public class GameCreateDTO
+    {
+        [Required(ErrorMessage = "Title is required")]
+        [MinLength(3, ErrorMessage = "Title must be at least 3 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Time is required")]
+        [FutureDate(ErrorMessage = "Time cannot be in the past")]
+        public DateTime Time { get; set; }
+
+        [Required(ErrorMessage = "TournamentId is required")]
+        public int TournamentId { get; set; }
+    }
+}
